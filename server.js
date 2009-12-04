1cede26ea5334243b8cb73bee2cf2cb17b3001ec
Jacek Becela <jacek.becela@gmail.com>
2009-12-04 11:42:38 +0100

    Tidy up style

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-var nano = function(template, data){
-  return template.replace(/\{([\w\.]*)}/g, function(str, key){
+var nano = function (template, data) {
+  return template.replace(/\{([\w\.]*)}/g, function (str, key) {
     var keys = key.split("."), value = data[keys.shift()];
-    for(i in keys){ value = value[keys[i]] };
+    keys.forEach(function (key) { value = value[key] });
     return value;
   });
 };
@@ -19,43 +19,42 @@ var sys = require("sys"),
   username = process.ARGV[2] || "ncr",
   count = process.ARGV[3] || 50,
   url = "http://flickr.com/photos/" + username;
-  sys.puts("* Spying for: " + url)
+
+sys.puts("* Spying for: " + url);
   
-flickr.get(nano(urls.user, {junk: junk, url: encodeURIComponent(url)}), {host: host}).finish(function(res){
+flickr.get(nano(urls.user, { junk: junk, url: encodeURIComponent(url) }), { host: host }).finish(function (res) {
   res.setBodyEncoding("utf8");
   var body = [];
-  res.addListener("body", function(chunk){
+  res.addListener("body", function (chunk) {
     body.push(chunk);
   });
-  res.addListener("complete", function(){
-    var user_id = JSON.parse(body.join("")).user.id
-    flickr.get(nano(urls.contacts, {junk: junk, user_id: user_id}), {host: host}).finish(function(res){
+  res.addListener("complete", function () {
+    var user_id = JSON.parse(body.join("")).user.id;
+    flickr.get(nano(urls.contacts, { junk: junk, user_id: user_id }), { host: host }).finish(function (res) {
       res.setBodyEncoding("utf8");
       var body = [];
-      res.addListener("body", function(chunk){
+      res.addListener("body", function (chunk) {
         body.push(chunk);
       });
-      res.addListener("complete", function(){
+      res.addListener("complete", function () {
         var clients = [];
         for(var i = 0; i < count; i++){
-          clients.push(http.createClient(80, host))
+          clients.push(http.createClient(80, host));
         }
-        JSON.parse(body.join("")).contacts.contact.forEach(function(c){
+        JSON.parse(body.join("")).contacts.contact.forEach(function (c) {
           var user_id = c.nsid;
           var flickr = clients[Math.floor(Math.random() * count)];
-          flickr.get(nano(urls.comments, {junk: junk, user_id: user_id}), {host: host}).finish(function(res){
+          flickr.get(nano(urls.comments, { junk: junk, user_id: user_id }), { host: host }).finish(function (res) {
             res.setBodyEncoding("utf8");
             var body = [];
-            res.addListener("body", function(chunk){
+            res.addListener("body", function (chunk) {
               body.push(chunk);
             });
-            res.addListener("complete", function(){
-              var first = JSON.parse(body.join("")).items[0]
-              if(first){
-                sys.puts(first.link)
-              }
+            res.addListener("complete", function () {
+              var first = JSON.parse(body.join("")).items[0];
+              if(first){ sys.puts(first.link) };
             });
-          })
+          });
         });
       });
     });
