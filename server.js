7e9e95d6553b5169c8660ed8768a583e165e8822
Jacek Becela <jacek.becela@gmail.com>
2009-12-04 11:17:20 +0100

    Add my api key, parallelize requests

--- server.js
+++ server.js
@@ -9,15 +9,17 @@ var nano = function(template, data){
 var sys = require("sys"), 
   http = require("http"),
   host = "api.flickr.com",
-  junk = "api_key=46c1e3ac7da832f729986bcc378abdc0&format=json&nojsoncallback=1",
+  junk = "api_key=3f6ad5daec2a6a194c54f19ea38dbe5d&format=json&nojsoncallback=1",
   urls = {
     user: "/services/rest/?method=flickr.urls.lookupUser&{junk}&url={url}",
     contacts: "/services/rest/?method=flickr.contacts.getPublicList&{junk}&user_id={user_id}",
     comments: "/services/feeds/photos_comments.gne?{junk}&user_id={user_id}"
   },
   flickr = http.createClient(80, host),
-  username = process.ARGV[2] || "ncr"
+  username = process.ARGV[2] || "ncr",
+  count = process.ARGV[3] || 50,
   url = "http://flickr.com/photos/" + username;
+  sys.puts("* Spying for: " + url)
   
 flickr.get(nano(urls.user, {junk: junk, url: encodeURIComponent(url)}), {host: host}).finish(function(res){
   res.setBodyEncoding("utf8");
@@ -27,7 +29,6 @@ flickr.get(nano(urls.user, {junk: junk, url: encodeURIComponent(url)}), {host: h
   });
   res.addListener("complete", function(){
     var user_id = JSON.parse(body.join("")).user.id
-    // sys.puts("user_id: " + user_id)
     flickr.get(nano(urls.contacts, {junk: junk, user_id: user_id}), {host: host}).finish(function(res){
       res.setBodyEncoding("utf8");
       var body = [];
@@ -35,9 +36,13 @@ flickr.get(nano(urls.user, {junk: junk, url: encodeURIComponent(url)}), {host: h
         body.push(chunk);
       });
       res.addListener("complete", function(){
+        var clients = [];
+        for(var i = 0; i < count; i++){
+          clients.push(http.createClient(80, host))
+        }
         JSON.parse(body.join("")).contacts.contact.forEach(function(c){
           var user_id = c.nsid;
-          // sys.puts(user_id);
+          var flickr = clients[Math.floor(Math.random() * count)];
           flickr.get(nano(urls.comments, {junk: junk, user_id: user_id}), {host: host}).finish(function(res){
             res.setBodyEncoding("utf8");
             var body = [];
